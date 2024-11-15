67c5eb47445eeeb3f763b30001f6f7903ddd142d
Jaiswal Vikas <jaiswalvikas7276@gmail.com>
2024-11-15 18:20:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
-document.getElementById('fname').addEventListener('blur',function name() {
-	 this.value = this.value.toUpperCase();
+document.getElementById('fname').eventListener('blur',function(){
+	this.value=this.value.toUpperCase();
 })
